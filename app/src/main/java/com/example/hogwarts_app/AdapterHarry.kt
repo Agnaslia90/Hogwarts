3bef048bdbadc568bcbbd7628a3c0eacfacf71a0
NataliaAlv <natalia.alvarez@secture.com>
2024-11-30 20:33:51 +0100

    Created houses

--- app/src/main/java/com/example/hogwarts_app/AdapterHarry.kt
+++ app/src/main/java/com/example/hogwarts_app/AdapterHarry.kt
@@ -1,6 +1,7 @@
 package com.example.hogwarts_app
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,7 +10,7 @@ import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 
-class AdapterHarry (private val filmsList: List<String>, context: Context):
+class AdapterHarry(private val filmsList: List<String>, private val context: Context) :
     RecyclerView.Adapter<AdapterHarry.ViewHolder>() {
 
     val colorsHP = listOf(
@@ -19,14 +20,14 @@ class AdapterHarry (private val filmsList: List<String>, context: Context):
         ContextCompat.getColor(context, R.color.slytherin)
     )
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AdapterHarry.ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.items_layout,parent,false)
-        return ViewHolder(view)
+            .inflate(R.layout.items_layout, parent, false)
+        return ViewHolder(view, context)
     }
 
-    override fun onBindViewHolder(holder: AdapterHarry.ViewHolder, position: Int) {
-        holder.textView.text = filmsList[position]
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(filmsList[position], position)
 
         val color = colorsHP[position % colorsHP.size]
         holder.cardView.setCardBackgroundColor(color)
@@ -36,8 +37,36 @@ class AdapterHarry (private val filmsList: List<String>, context: Context):
         return filmsList.size
     }
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class ViewHolder(itemView: View, private val context: Context) : RecyclerView.ViewHolder(itemView) {
         val textView: TextView = itemView.findViewById(R.id.textView)
         val cardView: CardView = itemView.findViewById(R.id.cardView)
+
+        fun bind(house: String, position: Int) {
+            textView.text = house
+
+            cardView.setOnClickListener {
+                when (position) {
+                    0 -> {
+                        val intent1 = Intent(context, Gryffindor::class.java)
+                        context.startActivity(intent1)
+                    }
+
+                    1 -> {
+                        val intent1 = Intent(context, Hufflepuff::class.java)
+                        context.startActivity(intent1)
+                    }
+
+                    2 -> {
+                        val intent2 = Intent(context, Ravenclaw::class.java)
+                        context.startActivity(intent2)
+                    }
+
+                    3 -> {
+                        val intent3 = Intent(context, Slytherin::class.java)
+                        context.startActivity(intent3)
+                    }
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/hogwarts_app/Ravenclaw.kt
@@ -0,0 +1,4 @@
+package com.example.hogwarts_app
+
+class Ravenclaw {
+}
