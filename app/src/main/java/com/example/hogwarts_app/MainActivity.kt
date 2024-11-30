4f6c866419c6ce5277eeb8d260074cd06ba22b3e
NataliaAlv <natalia.alvarez@secture.com>
2024-11-30 20:10:37 +0100

    Updated activity and adapter created

--- app/src/main/java/com/example/hogwarts_app/MainActivity.kt
+++ app/src/main/java/com/example/hogwarts_app/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.hogwarts_app
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        actionBar?.hide()
+
+        val recyclerView: RecyclerView = findViewById (R.id.recyclerView)
+
+        val filmsHP = listOf("Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin")
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = AdapterHarry(filmsHP, this)
     }
 }
