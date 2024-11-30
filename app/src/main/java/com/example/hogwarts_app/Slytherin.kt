e10996fa3e3d4609ba92c447c88a8d575bb5cda3
NataliaAlv <natalia.alvarez@secture.com>
2024-11-30 22:05:20 +0100

    Created all houses

--- app/src/main/java/com/example/hogwarts_app/Slytherin.kt
+++ app/src/main/java/com/example/hogwarts_app/Slytherin.kt
@@ -1,15 +1,25 @@
 package com.example.hogwarts_app
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class Slytherin: AppCompatActivity() {
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.slytherin_activity)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
         supportActionBar?.hide()
+
+        val btnBack = findViewById<Button>(R.id.btnBack)
+        btnBack.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
