13e406925c4f65b63942da756dbfdf4000bb7abc
NataliaAlv <natalia.alvarez@secture.com>
2024-12-03 09:15:26 +0100

    Created sorting hat

--- app/src/main/java/com/example/hogwarts_app/MainActivity.kt
+++ app/src/main/java/com/example/hogwarts_app/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.hogwarts_app
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,5 +23,19 @@ class MainActivity : AppCompatActivity() {
 
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = AdapterHarry(filmsHP, this)
+
+        val btnHouses: Button = findViewById(R.id.selectHouseBtn)
+        val selectedHouse: TextView = findViewById(R.id.textViewSelected)
+
+        btnHouses.setOnClickListener{
+            val houseResult= filmsHP.random()
+            selectedHouse.text = "Tu casa es: $houseResult"
+            when (houseResult) {
+                "Gryffindor" -> selectedHouse.setTextColor(getColor(R.color.griffindor))
+                "Hufflepuff" -> selectedHouse.setTextColor(getColor(R.color.hufflepuff))
+                "Ravenclaw" -> selectedHouse.setTextColor(getColor(R.color.ravenclaw))
+                "Slytherin" -> selectedHouse.setTextColor(getColor(R.color.slytherin))
+            }
+        }
     }
 }
